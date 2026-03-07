5dd059cfd7349b3db04fdb4d46d23086f30c343d
Weird Fish <malachilotr3019@gmail.com>
2026-03-06 20:53:11 -0600

    for now, nothing happens on button press

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function confirm() {
-    const modal = document.getElementById('modal');
-    const success = document.getElementById('success');
-    modal.style.animation = 'none';
-    modal.style.transition = 'opacity 0.25s, transform 0.25s';
-    modal.style.opacity = '0';
-    modal.style.transform = 'scale(0.9)';
-    setTimeout(() => {
-    modal.style.display = 'none';
-    success.style.display = 'flex';
-    }, 260);
+    // const modal = document.getElementById('modal');
+    // const success = document.getElementById('success');
+    // modal.style.animation = 'none';
+    // modal.style.transition = 'opacity 0.25s, transform 0.25s';
+    // modal.style.opacity = '0';
+    // modal.style.transform = 'scale(0.9)';
+    // setTimeout(() => {
+    // modal.style.display = 'none';
+    // success.style.display = 'flex';
+    // }, 260);
 }
 
 function deny() {
-    history.back();
-    // Fallback if there's no history
-    setTimeout(() => {
-        window.location.href = 'about:blank';
-    }, 100);
+    // history.back();
+    // // Fallback if there's no history
+    // setTimeout(() => {
+    //     window.location.href = 'about:blank';
+    // }, 100);
 }
 
 
